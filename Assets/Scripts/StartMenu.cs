d955bbb07899e8911ec80cc8a3889e8bffccbbbf
AyakorK <guillaume.moret@yahoo.com>
2023-11-14 21:30:23 +0100

    feat: Add Music on the Main Menu

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -5,23 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+
+    [Header("References")]
+    AudioClip WelcomeMusic_FX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        WelcomeMusic_FX = Resources.Load<AudioClip>("WelcomeMusic");
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = WelcomeMusic_FX;
+        audioSource.loop = true;
+        audioSource.Play();
     }
 
     public void StartGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void ExitGame() {
         Application.Quit();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
