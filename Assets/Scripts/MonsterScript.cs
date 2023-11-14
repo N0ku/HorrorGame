2aa3c58fb204d575930c7b4979ae8d7c787039ee
Nakku <naku.pocket@gmail.com>
2023-11-14 10:52:48 +0100

    feat/finish path finding for monsters

--- Assets/Scripts/MonsterScript.cs
+++ Assets/Scripts/MonsterScript.cs
@@ -1,28 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterScript : MonoBehaviour
 {
-    public GameObject Player;
-    public GameObject Monster;
-
-    private float speed = 0.5f; // increase based on playerStress ?
+    public Transform player;
+    private NavMeshAgent monster;
     private float playerStress = 0;
-
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        monster = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Monster.transform.position = Vector3.MoveTowards(Monster.transform.position, Player.transform.position, speed * Time.deltaTime);   
+        monster.destination = player.position;
     }
 
-   
+
 }
