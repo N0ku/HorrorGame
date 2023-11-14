165e490f2bdc0e3053ae100d2148ca8e78303b15
Nakku <naku.pocket@gmail.com>
2023-11-14 11:20:47 +0100

    feature/add if is looking by the camera it goes away from the player

--- Assets/Scripts/MonsterScript.cs
+++ Assets/Scripts/MonsterScript.cs
@@ -7,6 +7,9 @@ public class MonsterScript : MonoBehaviour
 {
     public Transform player;
     private NavMeshAgent monster;
+
+    public Camera camera;
+    public GameObject target;
     private float playerStress = 0;
 
 
@@ -19,8 +22,34 @@ public class MonsterScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        monster.destination = player.position;
+
+        var targetRender = target.GetComponent<Renderer>();
+
+        if (isLooking(camera, target))
+        {
+            targetRender.material.color = Color.red;
+            monster.destination = player.position.normalized * -1;
+            monster.speed = 0.5f;
+        }
+        else
+        {
+            targetRender.material.color = Color.white;
+            monster.destination = player.position;
+            monster.speed = 1 + playerStress / 100;
+        }
     }
 
+    private bool isLooking(Camera c, GameObject target)
+    {
+        var planes = GeometryUtility.CalculateFrustumPlanes(c);
+        var point = target.transform.position;
+
+        foreach (var plane in planes)
+        {
+            if (plane.GetDistanceToPoint(point) < 0)
+                return false;
+        }
+        return true;
+    }
 
 }
