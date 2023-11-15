e7274564922a903f920df12d2a5bb932f0703e37
AyakorK <guillaume.moret@yahoo.com>
2023-11-15 16:51:54 +0100

    feat: Fix build and comment a code that made it crash

--- Assets/Editors/FieldOfViewEditor.cs
+++ Assets/Editors/FieldOfViewEditor.cs
@@ -1,37 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEditor;
 
-[CustomEditor(typeof(MonsterScript))]
-public class FieldOfViewEditor : Editor
-{
-    private void OnSceneGUI()
-    {
-        MonsterScript fov = (MonsterScript)target;
-        Handles.color = Color.white;
-        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.radius);
+// [CustomEditor(typeof(MonsterScript))]
+// public class FieldOfViewEditor : Editor
+// {
+//     private void OnSceneGUI()
+//     {
+//         MonsterScript fov = (MonsterScript)target;
+//         Handles.color = Color.white;
+//         Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.radius);
 
-        Vector3 viewAngleA = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.angle / 2);
-        Vector3 viewAngleB = DirectionFromAngle(fov.transform.eulerAngles.y, fov.angle / 2);
+//         Vector3 viewAngleA = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.angle / 2);
+//         Vector3 viewAngleB = DirectionFromAngle(fov.transform.eulerAngles.y, fov.angle / 2);
 
-        Handles.color = Color.yellow;
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.radius);
-        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.radius);
+//         Handles.color = Color.yellow;
+//         Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.radius);
+//         Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.radius);
 
-        if (fov.canSeePlayer)
-        {
-            Handles.color = Color.red;
-            Handles.DrawLine(fov.transform.position, fov.playerObj.transform.position);
-        }
+//         if (fov.canSeePlayer)
+//         {
+//             Handles.color = Color.red;
+//             Handles.DrawLine(fov.transform.position, fov.playerRef.transform.position);
+//         }
 
-    }
+//     }
 
 
-    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
-    {
-        angleInDegrees += eulerY;
-        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
-    }
+//     private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
+//     {
+//         angleInDegrees += eulerY;
+//         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+//     }
 
-}
+// }
